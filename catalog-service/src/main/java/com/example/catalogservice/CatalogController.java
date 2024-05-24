9d5b458b3438f9a82ed4768eefc34aab5f9f3efa
dwjung <dev.dwjung@gmail.com>
2024-05-24 17:14:00 +0900

    gateway 프로젝트 추가 및 controller 수정

--- catalog-service/src/main/java/com/example/catalogservice/CatalogController.java
+++ catalog-service/src/main/java/com/example/catalogservice/CatalogController.java
@@ -1,10 +1,14 @@
+package com.example.catalogservice;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/catalogs")
 public class CatalogController {
 
-    @GetMapping("/catalog")
+    @GetMapping("")
     public String find(){
         return "Hello Catalog Service ";
     }
--- user-service/src/main/java/com/example/userservice/UserController.java
+++ user-service/src/main/java/com/example/userservice/UserController.java
@@ -1,10 +1,14 @@
+package com.example.userservice;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("/users")
+    @GetMapping("/welcome")
     public String find(){
         return "Hello User Service ";
     }
