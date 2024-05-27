f983311ca5bbcdd9d612aab6ef3932edbd962634
dwjung <dev.dwjung@gmail.com>
2024-05-27 14:09:50 +0900

    랜덤 포트와 라우팅

--- user-service/src/main/java/com/example/userservice/UserController.java
+++ user-service/src/main/java/com/example/userservice/UserController.java
@@ -1,5 +1,10 @@
 package com.example.userservice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+    private final Environment env;
 
     @GetMapping("/welcome")
     public String find(){
@@ -17,6 +26,12 @@ public class UserController {
     @GetMapping("/message")
     public String msg(@RequestHeader("gateway") String gateway){
         return "gateway: "+gateway;
+    }
+
+    @GetMapping("/whois")
+    public String whois(HttpServletRequest request){
+        log.info("server port: {}", request.getServerPort());
+        return String.format("hi, this is %s port", env.getProperty("local.server.port"));
     }
 
 
