9fe6012a7d649f4dbf09fa5d6fa2e124a104e6b4
dwjung <dev.dwjung@gmail.com>
2024-05-27 11:06:56 +0900

    글로벌 필터 추가

--- gateway-service/src/main/java/com/example/gatewayservice/filter/FilterConfig.java
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/FilterConfig.java
@@ -1,29 +1,32 @@
 package com.example.gatewayservice.filter;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
 //@Configuration
-//public class FilterConfig {
-//
-//    // application.yml 파일로 설정했던 gateway의 routes 설정을 config 파일로 처리한다
+public class FilterConfig {
+
+    // application.yml 파일로 설정했던 gateway의 routes 설정을 config 파일로 처리한다
 //    @Bean
-//    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-//        return builder
-//                .routes()
-//                .route(
-//                        pre -> pre.path("/users/**")
-//                                .filters(f -> f.addRequestHeader("gateway", "hello user")
-//                                        .addResponseHeader("gateway", "bye user")
-//                                )
-//                                .uri("http://localhost:8081")
-//                )
-//                .route(
-//                        pre -> pre.path("/catalogs/**")
-//                                .filters(f -> f.addRequestHeader("gateway", "hello catalog")
-//                                        .addResponseHeader("gateway", "bye catalog")
-//                                )
-//                                .uri("http://localhost:8082")
-//                )
-//                .build();
-//    }
-//
-//
-//}
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(
+                        pre -> pre.path("/users/**")
+                                .filters(f -> f.addRequestHeader("gateway", "hello user")
+                                        .addResponseHeader("gateway", "bye user")
+                                )
+                                .uri("http://localhost:8081")
+                )
+                .route(
+                        pre -> pre.path("/catalogs/**")
+                                .filters(f -> f.addRequestHeader("gateway", "hello catalog")
+                                        .addResponseHeader("gateway", "bye catalog")
+                                )
+                                .uri("http://localhost:8082")
+                )
+                .build();
+    }
+
+
+}
---
+++ gateway-service/src/main/java/com/example/gatewayservice/filter/GlobalFilter.java
@@ -0,0 +1,49 @@
+package com.example.gatewayservice.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Config> {
+
+    @Data
+    public static class Config{
+        // yml 파일에 arg가 정의 되어 있다.
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+
+    public GlobalFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        // pre filter
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+            log.info("[Global Filter] message: {}", config.getBaseMessage());
+            if(config.isPreLogger()){
+                log.info("[Global PRE Global] request id: {}", request.getId());
+            }
+
+            // post filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if(config.isPostLogger()) {
+                    log.info("[Global POST Global] response status: {} ", response.getStatusCode());
+                }
+            }));
+        };
+    }
+
+
+}
