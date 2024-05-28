472837845a1c605c912540cdbeaac2a0efddffbc
dwjung <dev.dwjung@gmail.com>
2024-05-28 15:08:13 +0900

    디펜던시 추가 health check 수정 application.yml 수정

--- catalog-service/src/main/java/com/example/catalogservice/CatalogController.java
+++ catalog-service/src/main/java/com/example/catalogservice/CatalogController.java
@@ -1,5 +1,7 @@
 package com.example.catalogservice;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/catalogs")
+@RequiredArgsConstructor
 public class CatalogController {
 
-    @GetMapping("/welcome")
+    private final Environment env;
+
+    @GetMapping("/health_check")
     public String find(){
-        return "Hello Catalog Service ";
+        return String.format("Hello Catalog Service on %s", env.getProperty("local.server.port"));
     }
 
     @GetMapping("/message")
