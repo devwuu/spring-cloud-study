c683a680f2574bcd5be1c8b46516cf63dc83c231
dwjung <dev.dwjung@gmail.com>
2024-05-28 13:14:28 +0900

    spring security 추가

---
+++ user-service/src/main/java/com/example/userservice/security/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.userservice.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrfConfigurer -> csrfConfigurer.disable())
+                .authorizeHttpRequests(requestMatcherRegistry ->
+                        requestMatcherRegistry.requestMatchers("/users/**").permitAll()
+                                .requestMatchers("/h2-console/**").permitAll()
+                )
+                .headers(headersConfigurer -> 
+                        headersConfigurer.frameOptions(frameOptionsConfig -> 
+                                frameOptionsConfig.disable()));
+
+        return http.build();
+    }
+//
+//    @Bean
+//    public
+
+
+
+}
