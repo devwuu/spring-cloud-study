50d88e9de70a3ca63f1cae79edd80002d359418e
devwuu <dev.dwjung@gmail.com>
2024-06-25 20:46:33 +0900

    주석 추가

--- user-service/src/main/java/com/example/userservice/config/RestTemplateConfig.java
+++ user-service/src/main/java/com/example/userservice/config/RestTemplateConfig.java
@@ -1,5 +1,6 @@
 package com.example.userservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
     @Bean
+    @LoadBalanced // order-serivce를 유레카에 등록되어 있는 도메인으로 대체할 수 있게 한다
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
